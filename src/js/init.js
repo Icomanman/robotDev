9d3cc4eb43d36a2a504c133b6a1a28fc9786431b
icomanman <fred_barabs@yahoo.com>
2019-08-13 23:25:55 +0800

    added beam data extractor via api.

--- src/js/init.js
+++ src/js/init.js
@@ -2,11 +2,11 @@
 // main app to process values from csv file
 
 const fs = require("fs");
-const csv = require(csv-parser);
+const csv = require("csv-parser");
+const fcsv = require("fast-csv");
+const ws = fs.createWriteStream("out.csv");
 
-// const fastcsv = require('fast-csv');
-// const fs = require('fs');
-// const ws = fs.createWriteStream("out.csv");
 // fastcsv
 //   .write(data, { headers: true })
 //   .pipe(ws);
+	
