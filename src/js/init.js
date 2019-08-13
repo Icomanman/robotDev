fec70658757ab838d48af11c5f7b3ad383fbd581
icomanman <fred_barabs@yahoo.com>
2019-08-13 23:05:44 +0800

    changed csv package dependency.

--- src/js/init.js
+++ src/js/init.js
@@ -3,3 +3,10 @@
 
 const fs = require("fs");
 const csv = require(csv-parser);
+
+// const fastcsv = require('fast-csv');
+// const fs = require('fs');
+// const ws = fs.createWriteStream("out.csv");
+// fastcsv
+//   .write(data, { headers: true })
+//   .pipe(ws);
