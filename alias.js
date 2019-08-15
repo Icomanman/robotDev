c05ccaab54c97b20f927af8effb625b1bec13bf3
icomanman <fred_barabs@yahoo.com>
2019-08-15 15:55:59 +0800

    started adding basic robot api functionality using python.

--- alias.js
+++ alias.js
@@ -4,22 +4,24 @@ const dat_path = `${__dirname}/dat`;
 const src_path = `${__dirname}/src/js`;
 
 const location = file_name => {
-	
+
 	let dir = {
 		// data files:	
 		"dat.txt": `${dat_path}/dat.txt`,
-		"out.json":`${dat_path}/out.json`,
+		"out.json": `${dat_path}/out.json`,
 		// module files:
-		"input_proc.js":`${src_path}/input_proc`
+		"input_proc.js": `${src_path}/input_proc`
 	};
 
 	let path = `unknown file name: ${file_name}.`
-	if (dir.hasOwnProperty(file_name)){
+	if (dir.hasOwnProperty(file_name)) {
 		path = dir[file_name];
 	}
 	return path;
 };
 
 module.exports = {
-	location
+	location,
+	dat_path,
+	src_path
 }
