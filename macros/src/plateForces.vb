a2025f3ad0f07a0bb6c7c730af7ca453ad47dbfb
icomanman <fred_barabs@yahoo.com>
2019-09-03 22:58:24 +0800

    added plateFoces macro.

---
+++ macros/src/plateForces.vb
@@ -0,0 +1,230 @@
+
+
+Sub clr_range()
+
+    Range("A6:G6").Select
+    Range(Selection, Selection.End(xlDown)).Select
+    Selection.Value = ""
+    Range("A6").Select
+    
+End Sub
+
+Sub papaya_extract()
+
+    Dim rob As IRobotApplication
+    Dim str As IRobotStructure
+   
+    Dim panSel As IRobotSelection
+    Dim caseSel As IRobotSelection
+    Dim ulsCases As IRobotSelection
+    
+    Dim origin As Range
+    Dim dataRange As Range
+    
+    Dim panelNo As Long
+    Dim caseNo As Long
+    Dim totalCount As Long
+    Dim forces() As Double
+    Dim bool As Long
+    
+    Set rob = New RobotApplication
+
+    If Not rob.Visible Then
+        MsgBox "No instance of Robot was found.", vbCritical, "Robot API"
+        Debug.Print "Robot Flag: " & rob.Visible
+        Debug.Print "No instance of Robot was found. Exiting..."
+        Exit Sub
+    Else
+        If Not I_RST_AVAILABLE = 1 Then
+            MsgBox "Results are not available. Please run you calculations.", vbCritical, "Bobot"
+            Exit Sub
+        End If
+    End If
+    Set str = rob.Project.Structure
+    Set panSel = str.Selections.Get(I_OT_PANEL)
+    Debug.Print "Selected Panels: " & panSel.count
+    If panSel.count < 1 Then
+        MsgBox "Kulang ka ba sa puti?", vbCritical, "No Panel Element selected"
+        Exit Sub
+    End If
+    
+    ' Manual selection of load cases:
+    'Set caseSel = str.Selections.Get(I_OT_CASE)
+    
+    'matic selection based on this model ONLY
+    Set ulsCases = str.Selections.Create(I_OT_CASE)
+    'ulsCases.AddText ("1001to1149")
+    ulsCases.AddText ("11")
+    
+    Dim Res As IRobotResultQueryReturnType
+    Dim RobResQueryParams As RobotResultQueryParams
+    Dim RobResRowSet As RobotResultRowSet
+
+    Set RobResQueryParams = rob.CmpntFactory.Create(I_CT_RESULT_QUERY_PARAMS)
+    RobResQueryParams.Selection.Set I_OT_PANEL, panSel
+    RobResQueryParams.Selection.Set I_OT_CASE, ulsCases
+
+    Debug.Print "Selected Panel ID(s): " & panSel.ToText
+    'RobResQueryParams.SetParam I_RPT_LAYER, I_FLT_ABSOLUTE_MAXIMUM
+    'RobResQueryParams.SetParam I_RPT_BAR_DIV_COUNT, 5
+    RobResQueryParams.SetParam I_RPT_PANEL, 10074
+    'Debug.Print RobResQueryParams.IsParamSet(I_RPT_LOAD_CASE)
+    'Debug.Print RobResQueryParams.GetParam(I_RPT_PANEL)
+
+    RobResQueryParams.ResultIds.SetSize (4)
+    RobResQueryParams.ResultIds.Set 1, I_FRT_DETAILED_NXX '492
+    RobResQueryParams.ResultIds.Set 2, I_FRT_DETAILED_NYY '493
+    RobResQueryParams.ResultIds.Set 3, I_FRT_DETAILED_NXY '494
+    RobResQueryParams.ResultIds.Set 4, I_FRT_DETAILED_MXX '501
+    
+    Dim NXX As Double
+    Dim NYY As Double
+    Dim NXY As Double
+    Dim MXX As Double
+    
+    Dim xF(7, 5) As Double
+    Dim i As Integer
+    Dim j As Integer
+
+    ' Initialise values to 0:
+    For i = 0 To 7
+        For j = 1 To 4
+            xF(i, j) = 0
+        Next j
+    Next i
+    
+    Do
+        Set RobResRowSet = New RobotResultRowSet
+        Res = str.Results.Query(RobResQueryParams, RobResRowSet)
+        starter = RobResRowSet.MoveFirst()
+        If starter = False Then
+            Debug.Print "Early exit...results row set failed"
+            MsgBox "Early exit...results row set failed", vbCritical, "Bobot"
+            Exit Sub
+        End If
+        
+        While starter
+            NXX = RobResRowSet.CurrentRow.GetValue(RobResRowSet.ResultIds.Get(1))
+            NYY = RobResRowSet.CurrentRow.GetValue(RobResRowSet.ResultIds.Get(2))
+            NXY = RobResRowSet.CurrentRow.GetValue(RobResRowSet.ResultIds.Get(3))
+            MXX = RobResRowSet.CurrentRow.GetValue(RobResRowSet.ResultIds.Get(4))
+
+        ' Set proper values:
+            ' NXX Min
+            If xF(0, 1) > NXX Then
+                xF(0, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(0, 1) = NXX / 1000
+                xF(0, 2) = NYY / 1000
+                xF(0, 3) = NXY / 1000
+                xF(0, 4) = MXX / 1000
+                xF(0, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+            ' NXX Max
+            If xF(1, 1) < NXX Then
+                xF(1, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(1, 1) = NXX / 1000
+                xF(1, 2) = NYY / 1000
+                xF(1, 3) = NXY / 1000
+                xF(1, 4) = MXX / 1000
+                xF(1, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+            ' NYY Min
+            If xF(2, 2) > NYY Then
+                xF(2, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(2, 1) = NXX / 1000
+                xF(2, 2) = NYY / 1000
+                xF(2, 3) = NXY / 1000
+                xF(2, 4) = MXX / 1000
+                xF(2, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+            ' NYY Max
+            If xF(3, 2) < NYY Then
+                xF(3, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(3, 1) = NXX / 1000
+                xF(3, 2) = NYY / 1000
+                xF(3, 3) = NXY / 1000
+                xF(3, 4) = MXX / 1000
+                xF(3, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+            ' NXY Min
+            If xF(4, 3) > NXY Then
+                xF(4, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(4, 1) = NXX / 1000
+                xF(4, 2) = NYY / 1000
+                xF(4, 3) = NXY / 1000
+                xF(4, 4) = MXX / 1000
+                xF(4, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+            ' NXY Max
+            If xF(5, 3) < NXY Then
+                xF(5, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(5, 1) = NXX / 1000
+                xF(5, 2) = NYY / 1000
+                xF(5, 3) = NXY / 1000
+                xF(5, 4) = MXX / 1000
+                xF(5, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+            ' MXX Min
+            If xF(6, 4) > MXX Then
+                xF(6, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(6, 1) = NXX / 1000
+                xF(6, 2) = NYY / 1000
+                xF(6, 3) = NXY / 1000
+                xF(6, 4) = MXX / 1000
+                xF(6, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+            ' MXX Max
+            If xF(7, 4) < MXX Then
+                xF(7, 0) = RobResRowSet.CurrentRow.GetParam(I_RPT_PANEL)
+                xF(7, 1) = NXX / 1000
+                xF(7, 2) = NYY / 1000
+                xF(7, 3) = NXY / 1000
+                xF(7, 4) = MXX / 1000
+                xF(7, 5) = RobResRowSet.CurrentRow.GetParam(I_RPT_LOAD_CASE)
+            End If
+        starter = RobResRowSet.MoveNext()
+        Wend
+
+    Loop While Res = I_RQRT_MORE_AVAILABLE
+    
+    Set origin = Range("I6")
+    Set dataRange = Range(origin, Range("N13"))
+    dataRange.Value = xF
+ 
+    Set RobResQueryParams = Nothing
+    Set ulsCases = Nothing
+    Set caseSel = Nothing
+    Set membSel = Nothing
+    Set str = Nothing
+    Set rob = Nothing
+    Set dataRange = Nothing
+    Set origin = Nothing
+ 
+End Sub
+
+Function verify_count(ByVal count As Long)
+    
+    Dim msg As String
+    
+    If count = Range("J17").Value Then
+        msg = "Data range is verified. Ok."
+    Else
+        msg = "Error. Please Check Data Range. IGOT!!!"
+        MsgBox msg
+    End If
+    
+End Function
+
+
+Sub clear_output()
+
+    Range("I6: N13").Value = ""
+
+End Sub
+
+
+Sub papaya_holder()
+    
+    MsgBox "Currently not working; function shifted to 'Extract Summary'", vbInformation, "Papaya!"
+
+End Sub
